0727396ffd4a8f7650470b475b7548ae4a9cbbf9
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-07-06 18:33:29 +0300

    Add files via upload

---
+++ Tuple_1.py
@@ -0,0 +1,12 @@
+def tpl_sort(tpl):
+  for element in tpl:
+    if isinstance(element, int):
+      return print(tpl)
+    else:
+      return print(tuple(sorted(tpl)))
+y = 'y'
+while y == 'y':
+  input_list = input('enter tuple of elements, using comma: ').split(',')
+  tpl = tuple(input_list)
+  tpl_sort(tpl)
+  y = input('restart?(y/n): ')
