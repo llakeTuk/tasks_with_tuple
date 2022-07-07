361e9dd12a005e61f83c5a1037c5d953f9698781
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-07-07 11:48:38 +0300

    Add files via upload

---
+++ Tuple_2.py
@@ -0,0 +1,18 @@
+def slicing(tpl, element):
+  if element in tpl:
+    if tpl.count(element) > 1:
+      first_index = tpl.index(element)
+      second_index = tpl.index(element, first_index + 1) + 1
+      return tpl[first_index : second_index]
+    else:
+      return tpl[tpl.index(element) :]
+  else:
+    return ()
+y = 'y'
+while y == 'y':
+  input_list = input('enter tuple of elements, using comma: ').split(',')
+  element = input('enter element for slicing: ')
+  tpl = tuple(input_list)
+  print(slicing(tpl, element))
+  y = input('restart?(y/n): ')
+print(goodbye)
