95d541695a6a8784201a2fd2fca0af76023b04f7
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-08-22 20:05:58 +0300

    Update Tuple_4.2.py

--- Tuple_4.2.py
+++ Tuple_4.2.py
@@ -3,6 +3,7 @@ def del_element_from_tpl(tpl, elem):
     elem_index = tpl.index(elem)
     return tpl[:elem_index] + tpl[elem_index + 1:]
   return tpl
+
 y = 'y'
 while y == 'y':
   input_tpl = input('enter tuple of elements, using comma: ').split(',')
